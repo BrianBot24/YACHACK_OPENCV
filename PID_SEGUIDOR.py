97574235173aea56f73ce8b86579b326ba9035c8
Walter Brayan Castillo Alayo <brianbot24@gmail.com>
2021-03-20 10:58:28 -0500

    Update PID_SEGUIDOR.py

--- PID_SEGUIDOR.py
+++ PID_SEGUIDOR.py
@@ -6,16 +6,16 @@ derivativo= 0
 proporcional= 0
 integral= 0
 PID_= 0
-lerror= 140 # para fines practicos se le da un valor, se inicia con 0 en el codigo real
+lerror= 140         # Para fines practicos se le da un valor, se inicia con 0 en el codigo real
 
-cX= 438 #valor de error(Se halla con el centro de la linea o centro de masa (findContours de OPENCV))
+cX= 438              #valor de error(Se halla con el centro de la linea o centro de masa (contorno, _ = cv2.findContours)
 
-vi= 70 # velocidad MAX de los motores en linea recta, puede ser de 40 a 80
+vi= 70               # velocidad MAX de los motores en linea recta, puede ser de 40 a 80
 Kp= (100-vi)/middleX # valor entre 0.096 a 0.032
-Kd= 0.0167 # valor apra fin demostrativo, se tiene que calibrar en cada robot
-Ki= 0.002  # valor apra fin demostrativo, se tiene que calibrar en cada robot
+Kd= 0.0167           # valor para fin demostrativo, se tiene que calibrar en cada tipo robot
+Ki= 0.002            # valor para fin demostrativo, se tiene que calibrar en cada tipo robot
 
-error= cX - middleX # error entre el pixel central con el de la linea negra
+error= cX - middleX  # error se halla entre el pixel central con el de la linea negra
 
 proporcional= error
 integral= integral + lerror
@@ -29,7 +29,7 @@ derivativo= error - lerror
 
 PID_= proporcional*Kp + integral*Ki + derivativo*Kd
 
-mdr= vi - PID_ #velocidad de motor derecho
+mdr= vi - PID_ #Velocidad de motor derecho
 miz= vi + PID_ #Velocidad de motor izquierda
 
 lerror = proporcional
