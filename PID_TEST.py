bd9a44fb361e5119314b2af5509993d3ca717b8f
Walter Brayan Castillo Alayo <brianbot24@gmail.com>
2021-03-28 12:25:39 -0500

    Update PID_TEST.py

--- PID_TEST.py
+++ PID_TEST.py
@@ -1,20 +1,20 @@
-middleX= 620 #Centro de la imagen (para el caso de mi camaraweb es de 1240x720)
+middleX= 620 #Center of the image (my webcam is 1240x720)
 
-#VALORES INICIALES
+#INIT VALUES
 derivativo= 0
 proporcional= 0
 integral= 0
 PID_= 0
-lerror= 140 # Para fines practicos se le da un valor, se inicia con 0 en el codigo real
+lerror= 140 #For practical purposes it is given a value, it starts with 0 in the real code
 
-cX= 466 #Valor de error(Se halla con el centro de la linea o centro de masa (findContours de OPENCV))
+cX= 466 #Value (Found with the center of the line or center of mass (findContours de OPENCV))
 
-vi= 70 # velocidad MAX de los motores en linea recta, puede ser de 40 a 80
-Kp= (100-vi)/middleX # valor entre 0.096 a 0.032
-Kd= 0.0167 # valor apra fin demostrativo, se tiene que calibrar en cada robot
-Ki= 0.002  # valor apra fin demostrativo, se tiene que calibrar en cada robot
+vi= 70               #MAX speed of motors in a straight line, can be from 40 to 80
+Kp= (100-vi)/middleX #Value between 0.096 to 0.032
+Kd= 0.0167 #Value for demonstration purposes, it has to be calibrated in each robot
+Ki= 0.002  #Value for demonstration purposes, it has to be calibrated in each robot
 
-error= cX - middleX # error entre el pixel central con el de la linea negra
+error= cX - middleX # 
 
 proporcional= error
 integral= integral + lerror
@@ -28,8 +28,8 @@ derivativo= error - lerror
 
 PID_= proporcional*Kp + integral*Ki + derivativo*Kd
 
-mdr= vi - PID_ #velocidad de motor derecho
-miz= vi + PID_ #Velocidad de motor izquierda
+mdr= vi - PID_ #Right engine speed
+miz= vi + PID_ #Left engine speed
 
 lerror = proporcional
 
