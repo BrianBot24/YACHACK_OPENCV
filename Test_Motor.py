54f7c18026237929a512a793b909ba9e15eae00b
Walter Brayan Castillo Alayo <brianbot24@gmail.com>
2021-03-29 23:09:16 -0500

    Create Test_Motor.py

---
+++ Test_Motor.py
@@ -0,0 +1,25 @@
+ from motor import *
+ from time import sleep
+
+vl1= 60
+vl2= 60
+
+Stop()
+sleep(1)
+avanzar(vl1,vl2)
+sleep(1)
+
+Stop()
+sleep(1)
+izquierda(vl1,vl2)
+sleep(1)
+
+Stop()
+sleep(1)
+derecha(vl1,vl2)
+sleep(1)
+
+Stop()
+sleep(1)
+retroceso(vl1,vl2)
+sleep(1)
